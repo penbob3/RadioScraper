e0353c696ebcc5a04b6baa2a2f1b7c1d0880f02f
Max <plazmacatchermc@gmail.com>
2020-05-07 22:12:25 +1000

    Added token authorization

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ const iplocation = require('iplocation');
 var express = require('express');
 var app = express();
 var port = process.env.PORT;
+const token = process.env.TOKEN;
 const productionPort = 80;
 
 if (port == undefined) {
@@ -37,56 +38,61 @@ async function initBrowser() {
     
     app.get('/currentsong', async (req, res) => {
       var finalResponse;
-      try {
-        await page.reload({ waitUntil: ["networkidle2", "domcontentloaded"]});
-        nowPlaying = parse.parse(await page.$eval('.item-current', e => e.innerHTML));
-        songJson.title = nowPlaying.querySelector('.title').text;
-        songJson.artist = nowPlaying.querySelector('.artist').text;
-        finalResponse = songJson;
-      }
-      catch{
-        songJson.title = "ERROR! Contact APTheHunter#8738";
-        songJson.artist = "ERROR! Contact APTheHunter#8738";
-        console.log("Website reload error! Did Pupp break, or is the website down?");
-      }
-      finally {
-        res.send(finalResponse);
-        if (port == productionPort) {
-          var location = await iplocation(req.ip);
-          console.log("Request from " + location.region.name + ", " + location.country.name + " (" + req.ip + ") recieved and fulfilled.")
-        } else {
-          console.log("Local request recieved and fulfilled.")
+      if (req.header('token') == token) {
+        try {
+          await page.reload({ waitUntil: ["networkidle2", "domcontentloaded"]});
+          nowPlaying = parse.parse(await page.$eval('.item-current', e => e.innerHTML));
+          songJson.title = nowPlaying.querySelector('.title').text;
+          songJson.artist = nowPlaying.querySelector('.artist').text;
+          finalResponse = songJson;
         }
+        catch{
+          songJson.title = "ERROR! Contact APTheHunter#8738";
+          songJson.artist = "ERROR! Contact APTheHunter#8738";
+          console.log("Website reload error! Did Pupp break, or is the website down?");
+        }
+        finally {
+          res.send(finalResponse);
+          if (port == productionPort) {
+            var location = await iplocation(req.ip);
+            console.log("Request from " + location.region.name + ", " + location.country.name + " (" + req.ip + ") recieved and fulfilled.")
+          } else {
+            console.log("Local request recieved and fulfilled.")
+          }
+        }
+      } else {
+        res.status(401)
+           .send('Invalid or missing token in header');
       }
     });
 
     app.get('/prevsong', async (req, res) => {
       var finalResponse;
-      try {
-        await page.reload({ waitUntil: ["networkidle2", "domcontentloaded"]});
-        nowPlaying = parse.parse(await page.$eval('.item-previous', e => e.innerHTML));
-        songJson.title = nowPlaying.querySelector('.title').text;
-        songJson.artist = nowPlaying.querySelector('.artist').text;
-        finalResponse = songJson;
-      }
-      catch(e){
-        songJson.title = "ERROR! Contact APTheHunter#8738";
-        songJson.artist = "ERROR! Contact APTheHunter#8738";
-        console.error(e);
-        console.log("Website reload error! Did Pupp break, or is the website down?");
-      }
-      finally {
-        res.send(finalResponse);
-        if (port == productionPort) {
-          try {
+      if (req.header('token') == token) {
+        try {
+          await page.reload({ waitUntil: ["networkidle2", "domcontentloaded"]});
+          nowPlaying = parse.parse(await page.$eval('.item-previous', e => e.innerHTML));
+          songJson.title = nowPlaying.querySelector('.title').text;
+          songJson.artist = nowPlaying.querySelector('.artist').text;
+          finalResponse = songJson;
+        }
+        catch{
+          songJson.title = "ERROR! Contact APTheHunter#8738";
+          songJson.artist = "ERROR! Contact APTheHunter#8738";
+          console.log("Website reload error! Did Pupp break, or is the website down?");
+        }
+        finally {
+          res.send(finalResponse);
+          if (port == productionPort) {
             var location = await iplocation(req.ip);
             console.log("Request from " + location.region.name + ", " + location.country.name + " (" + req.ip + ") recieved and fulfilled.")
-          } catch {
-            console.log("Request from " + req.ip + " recieved and fulfilled. (Is ipapi down/overused?)")
+          } else {
+            console.log("Local request recieved and fulfilled.")
           }
-        } else {
-          console.log("Local request recieved and fulfilled.")
         }
+      } else {
+        res.status(401)
+           .send('Invalid or missing token in header');
       }
     });
     
